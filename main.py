6d74006d9e154bba028fcf94105ceebf6b83dbab
Kinz <96962258+minikids@users.noreply.github.com>
2026-04-01 07:22:51 -0600

    Add Discord bot commands for message and status control
    Implement Discord bot commands for message handling and status updates.

---
+++ main.py
@@ -0,0 +1,61 @@
+from discord.ext import commands
+import discord
+import os
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def saytca(ctx, message: str):
+    if str(ctx.message.author) == "kinz_gamer":
+        MAIN_GUILD = bot.get_guild(1469749041163272233)
+        channel = MAIN_GUILD.get_channel(1487473577107263588)
+        await channel.send(message)
+
+@bot.command()
+async def dnd(ctx, status: str):
+    if str(ctx.message.author) == "kinz_gamer":
+        status = discord.CustomActivity(name=status)
+        await bot.change_presence(status=discord.Status.do_not_disturb, activity=status)
+
+@bot.command()
+async def online(ctx, status: str):
+    if str(ctx.message.author) == "kinz_gamer":
+        status = discord.CustomActivity(name=status)
+        await bot.change_presence(status=discord.Status.online, activity=status)
+
+@bot.command()
+async def offline(ctx, status: str):
+    if str(ctx.message.author) == "kinz_gamer":
+        status = discord.CustomActivity(name=status)
+        await bot.change_presence(status=discord.Status.offline, activity=status)
+
+@bot.command()
+async def shutdown(ctx):
+    if str(ctx.message.author) == "kinz_gamer":
+        os._exit(0)
+
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+
+run = False
+
+
+@app.route("/")
+def main():
+    global run
+    if run == False:
+        threading.Thread(
+            target=bot.run,
+            args=(
+                "MTQ4ODY4MDQ1OTUzMjUwNTIzNA.GcezsN.OTwBkDHfUP0REo4jbAaEfulWm20DaLpc876_EM",
+            ),
+        ).start()
+        run = True
+    return "Running"
+
+
+if __name__ == "__main__":
+    app.run(port=80, host="0.0.0.0")
